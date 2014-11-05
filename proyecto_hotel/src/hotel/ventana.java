49f7e7ee19f2af0e9486b3709306e4ed3c2cc268
sulecioalex <sulecioalex@hotmail.es>
2014-11-04 20:56:17 -0600

    final sin jcalander
    no funciona el calendario

--- proyecto_hotel/src/hotel/ventana.java
+++ proyecto_hotel/src/hotel/ventana.java
@@ -22,6 +22,7 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import com.toedter.calendar.JDateChooser;
 
 public class ventana extends JFrame {
 	
@@ -29,14 +30,33 @@ public class ventana extends JFrame {
 	static ArrayList<String> doc = new ArrayList<String>();
 	 static ArrayList<String> doc1 = new ArrayList<String>();
 	 static ArrayList<String> doc2 = new ArrayList<String>();
+	 static ArrayList<String> doc3 = new ArrayList<String>();
 	 static ArrayList hot=new ArrayList();
 	 static ArrayList fac=new ArrayList();
+	 static ArrayList pic=new ArrayList();
 	 static ArrayList<String> ndpi=new ArrayList<String>();
 	 
 	/**
 	 * Launch the application.
 	 */
-	
+		public static void combertir3() {
+			int estado=0;
+			int cuarto=0;
+			String l1="";
+			
+			for(int x=0;x<=551;x++){
+			StringTokenizer st=new StringTokenizer(doc3.get(x));
+		while (st.hasMoreTokens()){ // hacemos linea por linea linea 1
+			l1=st.nextToken();	
+			estado=Integer.parseInt(l1);
+			pic.add(estado);
+			}
+		}
+		
+		}
+	 
+	 
+	 
 	 
 	 public static void cargar (){
 			try{
@@ -125,6 +145,27 @@ public class ventana extends JFrame {
  }						
 }	
 	
+	public static void cargar3 (){
+		try{
+     // Abrimos el archivo
+     FileInputStream fstream = new FileInputStream("C:\\Users\\alex\\Desktop\\picina.txt");
+     // Creamos el objeto de entrada
+     DataInputStream entrada = new DataInputStream(fstream);
+     // Creamos el Buffer de Lectura
+     BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
+     String strLinea;
+     // Leer el archivo linea por linea
+     
+     while ((strLinea = buffer.readLine()) != null)   {
+         // Imprimimos la línea por pantalla
+           doc3.add(strLinea);
+     }
+     // Cerramos el archivo
+     entrada.close();
+ }catch (Exception e){ //Catch de excepciones
+     System.err.println("Ocurrio un error: " + e.getMessage());
+ }						
+}	
 	
 	
 	
@@ -160,11 +201,12 @@ public class ventana extends JFrame {
 		File archivo=new File("C:\\Users\\alex\\Desktop\\hotel0.txt");
 		File nom=new File("C:\\Users\\alex\\Desktop\\nombre1.txt");
 		File fact=new File("C:\\Users\\alex\\Desktop\\factura.txt");
+		File picin=new File("C:\\Users\\alex\\Desktop\\picina.txt");
 		//Crear objeto FileWriter que sera el que nos ayude a escribir sobre archivo
 		FileWriter escribir=new FileWriter(archivo,false);
 		FileWriter dp=new FileWriter(nom,false);
 		FileWriter dr=new FileWriter(fact,false);
-		
+		FileWriter dw=new FileWriter(picin,false);
 
 		for (int w=0;w<=1103;w++){
 			escribir.write(" "+(int) hot.get(w)+" " +hot.get(w+1)+" " );
@@ -177,6 +219,9 @@ public class ventana extends JFrame {
 		for (int e=0;e<=551;e++){
 			dr.write(" "+ fac.get(e));
 			dr.write("\r\n");
+			dw.write(" "+ pic.get(e));
+			dw.write("\r\n");
+			
 		}
 			
 
@@ -184,6 +229,7 @@ public class ventana extends JFrame {
 		escribir.close();
 		dp.close();
 		dr.close();
+		dw.close();
 		}
 
 		//Si existe un problema al escribir cae aqui
@@ -230,7 +276,9 @@ public class ventana extends JFrame {
 			
 
 			JOptionPane.showMessageDialog(null,"Esta ocupada por "+ndpi.get(p)+"  no dpi  "+ ndpi.get(j) );
-	
+			//
+			JOptionPane.showMessageDialog(null,"y lleva un monto a facturar de  "+ fac.get(a-1) );
+			
 		}
 		else {
 	
@@ -273,6 +321,7 @@ public class ventana extends JFrame {
 			hot.set(j, 0);
 			ndpi.set(j, "------");
 			ndpi.set(p, "------");
+			pic.set(a-1, 0);
 			
 			
 	
@@ -304,11 +353,26 @@ public class ventana extends JFrame {
 				
 				String nombre= JOptionPane.showInputDialog("Introduzca su nombre","");
 				String dpi= JOptionPane.showInputDialog("Introduzca numero de dpi","");
-				
+
 				ndpi.set(le, nombre);
 				ndpi.set(se, dpi);
 				hot.set(se, 1);
 			
+				String pi= JOptionPane.showInputDialog("Uso a picina 1 si 0 No","");
+				int poc=Integer.parseInt(pi);
+				if (poc==1){
+					int j;
+					j=qe -1;
+					pic.set(j, 1);	
+				}
+				if (qe <= 150){
+					
+					fac.set(qe-1, 150);
+				}
+				else{
+					fac.set(qe-1, 250);
+				}
+				
 			}
 			
 			guardar();
@@ -325,9 +389,11 @@ public class ventana extends JFrame {
 		cargar();
 		cargar1();
 		cargar2();
+		cargar3();
 		combertir();
 		combertir1();
 		combertir2();
+		combertir3();
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -358,7 +424,7 @@ public class ventana extends JFrame {
 				reservar();
 			}
 		});
-		btnReservar.setBounds(10, 11, 89, 23);
+		btnReservar.setBounds(25, 111, 89, 23);
 		contentPane.add(btnReservar);
 		
 		JButton btnEstado = new JButton("Estado");
@@ -367,17 +433,17 @@ public class ventana extends JFrame {
 				estado();
 			}
 		});
-		btnEstado.setBounds(168, 11, 89, 23);
+		btnEstado.setBounds(122, 206, 89, 23);
 		contentPane.add(btnEstado);
 		
-		JButton btnSalid = new JButton("salida");
+		JButton btnSalid = new JButton("salida y facturacion");
 		btnSalid.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				salida();
 				guardar();
 			}
 		});
-		btnSalid.setBounds(291, 11, 89, 23);
+		btnSalid.setBounds(234, 206, 125, 23);
 		contentPane.add(btnSalid);
 		
 		JButton btnNewButton = new JButton("Restaurante");
@@ -387,8 +453,16 @@ public class ventana extends JFrame {
 				guardar();
 			}
 		});
-		btnNewButton.setBounds(291, 77, 101, 23);
+		btnNewButton.setBounds(304, 25, 101, 23);
 		contentPane.add(btnNewButton);
+		
+		JDateChooser dateChooser = new JDateChooser();
+		dateChooser.setBounds(25, 25, 81, 20);
+		contentPane.add(dateChooser);
+		
+		JDateChooser dateChooser_1 = new JDateChooser();
+		dateChooser_1.setBounds(25, 77, 81, 20);
+		contentPane.add(dateChooser_1);
 		
 		JLabel lblNewLabel = new JLabel("New label");
 		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\alex\\Desktop\\hotel.jpg"));
